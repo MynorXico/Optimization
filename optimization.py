adba5faad1df45bd8e99c5ad79f50d016d5fb346
Mynor Xico <mynorottoniel.xico2@gmail.com>
2017-12-13 16:02:30 -0600

    Physics inspired Annealing Optimization Algorithm

--- optimization.py
+++ optimization.py
@@ -126,3 +126,39 @@ def hillclimb(domain, costf):
 s = hillclimb(domain, schedulecost)
 print(schedulecost(s))
 printschedule(s)
+
+def annealingoptimize(domain, costf, T=10000.0, cool = 0.95, step = 1):
+    # Initialize the values randomly
+    vec = [float(random.randint(domain[i][0], domain[i][1])) for i in range(len(domain))]
+
+    while T > 0.1:
+        # Choose one of the indices+
+        i = random.randint(0, len(domain)-1)
+
+        # Choose a direction to change it
+        dir = random.randint(-step, step)
+
+        # Create a new list with one of the values changed
+        vecb = vec[:]
+        vecb[i] += dir
+        if vecb[i]<domain[i][0]: vecb[i] = int(domain[i][0])
+        elif vecb[i] > domain[i][1]: vecb[i] =int(domain[i][1])
+
+        # Calculate the current cost and the new cost
+        ea = costf(vec)
+        eb = costf(vecb)
+        p = pow(math.e, (-eb-ea)/T)
+
+
+        # Is it better, or does it make the probability
+        # cutoff?
+
+        if(eb<ea or random.random()<p):
+            vec = vecb
+
+        # Decrease the temperature
+        T = T*cool
+    return vec
+
+s = annealingoptimize(domain, schedulecost)
+print(schedulecost(s))
